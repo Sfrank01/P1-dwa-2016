57fd2a744201eecf21fe897df13e97a983fd8343
sfrank01 <sfrank01@g.harvard.edu>
2016-02-23 10:38:00 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+
+<HTML>
+
+<HEAD>
+
+<style type="text/css">
+    h1 {
+        color: blue;
+        text-decoration: bold;
+        font-size: 35px;
+    }
+
+    h2 {
+        color: gray;
+		font-size: 25px;
+		
+    }
+    h3 {
+        color: gray;
+		font-size: 20px;
+    }
+	a {
+        color: green;
+		font-size: 15px;
+    }
+	
+	img {
+    height: auto; 
+    width: auto; 
+    max-width: 300px; 
+    max-height: 300px;
+}
+	
+    </style>
+
+
+<TITLE>P1</TITLE>
+
+</HEAD>
+
+<BODY BGCOLOR="FFFFFF">
+
+<CENTER><h1>Welcome to my site</h1></CENTER>
+<CENTER><h2>Susan Frank</h2></CENTER>
+
+
+<CENTER><img src="images/susan.jpg"> </CENTER>
+
+<CENTER><h3>DWA-15  Spring 2016</h3></CENTER><br>
+
+
+
+<CENTER><h2>PROJECT 1<h2>
+        <h3>View:<span> <a href="URL of the web page to go to">Text that will be the link</a> <br> 
+		Github:<span> <a href="URL of the web page to go to">Text that will be the link</a></h3></CENTER>
+		
+
+
+</BODY>
+
+</HTML> 
